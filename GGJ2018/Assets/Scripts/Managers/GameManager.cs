b8db73d03793d9af557ec1a33fa8ba99b78e6a6d
Xyunn <maxime.devecchi@gmail.com>
2018-01-26 23:10:48 +0100

    Creating repository

---
+++ GGJ2018/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Class used to manage the game
+/// </summary>
+public class GameManager : Singleton<GameManager> {
+
+    #region Public_Attributes
+
+    #endregion
+
+    #region Private_Attributes
+
+    private ENUM_GAMESTATE m_gameState;
+
+    #endregion
+
+    #region Unity_Methods
+
+    #endregion
+
+    #region Methods
+
+    /// <summary>
+    /// Change the game state and modify all behavior in consequences
+    /// </summary>
+    /// <param name="p_newState">New game state</param>
+    public void changeGameState(ENUM_GAMESTATE p_newState)
+    {
+        m_gameState = p_newState;
+
+        switch (m_gameState)
+        {
+            default:
+                Debug.Log("/ ! \\ No game state defined for the value: " + m_gameState.ToString());
+                break;
+        }
+    }
+
+    #endregion
+
+}
