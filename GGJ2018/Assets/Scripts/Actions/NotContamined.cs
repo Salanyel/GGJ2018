c230ce23c2c0db6f108ddd98b3638b388d3a5177
Xyunn <maxime.devecchi@gmail.com>
2018-01-26 23:27:07 +0100

    Generic system for actions

---
+++ GGJ2018/Assets/Scripts/Actions/NotContamined.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotContamined : Actions {
+
+	override protected void Awake() {
+		_actionKey = InputData._ActionKeyboard;
+	}
+
+	override public void Action() {
+		Debug.Log("--- Not contamined action");
+	}
+}
---
+++ GGJ2018/Assets/Scripts/Global/InputData.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class InputData {
+
+	public const string _ActionKeyboard = "Action-keyboard";
+
+}
