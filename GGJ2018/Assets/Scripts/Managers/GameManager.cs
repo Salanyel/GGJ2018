973f72e37550a154193c733ae9fa5f40f91e550a
Xyunn <maxime.devecchi@gmail.com>
2018-01-27 12:58:00 +0100

    Fixing scene and game manager

--- GGJ2018/Assets/Scripts/Managers/GameManager.cs
+++ GGJ2018/Assets/Scripts/Managers/GameManager.cs
@@ -193,7 +193,6 @@ public class GameManager : Singleton<GameManager> {
 
 		for (int i = 0; i < _players.Length; ++i) {
 			Player player = GetPlayer(i);
-			Debug.Log ("--- new score[" + player.PlayerNumber + "] : " + player.Score);
 		}
 
 		//Display score
@@ -282,15 +281,15 @@ public class GameManager : Singleton<GameManager> {
 
 		for (int i = 1; i < _players.Length; ++i) {
 			float currentPlayerScore = _players[i].GetComponent<Player> ().Score;
-			Debug.Log (i + ": " + currentPlayerScore + " / " + score);
 
 			if (currentPlayerScore > score) {
 				score = currentPlayerScore;
 				indexWinner = i;
 			}
 		}
+
 		_winner = indexWinner;
-		Debug.Log ("---Winner: " + indexWinner);
+
 		foreach (Text text in _finalScorePanel.GetComponentsInChildren<Text>()) {
 			if (text.gameObject.name == "WinnerScore") {
 				text.text = "Score: " + score;
