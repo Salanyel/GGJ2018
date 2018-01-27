76b7e8fdd7a60ec7334a8bebb48680934d101988
Maxime de VECCHI <maxime.devecchi@gmail.com>
2018-01-27 21:03:34 +0100

    Retry button (#40)

---
+++ GGJ2018/Assets/Scripts/UI/UI_Levels.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UI_Levels : MonoBehaviour {
+	public void OnClickRetry() {
+		GameManager.Instance.ChangeGameState (ENUM_GAMESTATE.RESET);
+	}
+}
