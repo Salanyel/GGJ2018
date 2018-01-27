6830cfc93e41ef0fd570d2e9b4951fbf92460807
Xyunn <maxime.devecchi@gmail.com>
2018-01-27 04:18:26 +0100

    Main menu interaction

---
+++ GGJ2018/Assets/Scripts/UI/UI_MenuScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UI_MenuScene : MonoBehaviour {
+
+	public void OnClickPlayGame() {
+		SceneManager.LoadScene(1, LoadSceneMode.Single);
+	}
+
+	public void OnClickExitGame() {
+		Application.Quit();
+	}
+}
