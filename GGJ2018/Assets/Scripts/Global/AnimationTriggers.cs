9285d8e9189fe3d27c7c9c083668c4d87b4e8eab
ralphnicolas <35853339+ralphnicolas@users.noreply.github.com>
2018-01-27 15:04:27 +0100

    Playerpush (#26)
    * psuh
    
    * ajout action push
    
    * bordure niveau physique
    
    * bug rotation
    
    * ajout tapis roulant
    
    * conflit mainscene
    
    * prefab elevator
    
    * ajout chargement morve attack
    
    * cone detection
    
    * integration
    
    * integration mesh
    
    * integration le retour de la vengeance

---
+++ GGJ2018/Assets/Scripts/Global/AnimationTriggers.cs
@@ -0,0 +1,10 @@
+public static class AnimationTriggers {
+
+	public const string _speed = "speed";
+    public const string _isIll = "isIll";
+    public const string _prepareAttack = "prepareAttack";
+    public const string _attack = "Attack";
+	
+
+
+}
