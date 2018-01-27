67775a1eda449a903738ebd37fd493c51eb1233e
ralphnicolas <35853339+ralphnicolas@users.noreply.github.com>
2018-01-27 17:32:56 +0100

    Playerpush (#32)
    * psuh
    
    * ajout action push
    
    * bordure niveau physique
    
    * bug rotation
    
    * ajout tapis roulant
    
    * conflit mainscene
    
    * prefab elevator
    
    * ajout chargement morve attack
    
    * cone detection
    
    * integration
    
    * integration mesh
    
    * integration le retour de la vengeance
    
    * ajout gabarit

--- GGJ2018/Assets/Scripts/Actions/ColdAction.cs
+++ GGJ2018/Assets/Scripts/Actions/ColdAction.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ColdAction : PlayerActions {
 
-	private float _actionRange = 4f;
+	private float _actionRange = 8f;
 	GameObject _indicator;
 
 	bool _isCharging;
@@ -13,26 +13,23 @@ public class ColdAction : PlayerActions {
 	public float attackAngle = 40f;
 	float _startTime;
 	PlayerController _playerController;
+	GameObject gabaritGO;
+	Transform gabaritTransform;
 
 	override protected void Awake() {
 		lastAction = Time.time - coolDownTime;
 		coolDownTime = 0f;
 		
-		_indicator = GameObject.CreatePrimitive(PrimitiveType.Cube);
-		Destroy(_indicator.GetComponent<BoxCollider>());
-		_indicator.transform.SetParent(gameObject.transform);
-		_indicator.transform.localPosition = new Vector3(0f, 0f, _actionRange - 0.5f);
-		_indicator.transform.localScale = new Vector3(0.1f, 0.1f, _actionRange);
-		_indicator.SetActive(false);
-
 		_playerController = gameObject.GetComponent<PlayerController>();
+
+		GameObject g =  Instantiate(Resources.Load("GabaritColdPrefab",typeof(GameObject)), transform) as GameObject;
+		gabaritTransform = g.transform;
 
 		_isCharging = false;
 		_chargeAmount = 0f;
 	}
 
 	void Fire(){
-		_indicator.SetActive(true);
 		StartCoroutine(HideDisplay());
 
 		foreach(GameObject g in SendRay()){
@@ -44,17 +41,16 @@ public class ColdAction : PlayerActions {
 	//renvoie la liste des players touches
 	GameObject[] SendRay(){
 
-		Debug.Log("Morve distance " + (_actionRange * _chargeAmount).ToString());
-
 		List<GameObject> hitted = new List<GameObject>();
+		
 		for(int i=0; i < 8;i++){
 			RaycastHit hitInfo;
 			Vector3 direction = transform.forward;
 			direction = Quaternion.AngleAxis(attackAngle * (float)i/8f - (attackAngle/2f) ,Vector3.up) * direction;
 
-			Physics.Raycast(transform.position, direction, out hitInfo, _actionRange * _chargeAmount + 0.5f);
+			Physics.Raycast(transform.position, direction * _actionRange * _chargeAmount, out hitInfo, _actionRange * _chargeAmount);
 			
-			Debug.DrawRay(transform.position, direction,Color.yellow,4f);
+			Debug.DrawRay(transform.position, direction * _actionRange * _chargeAmount,Color.yellow,4f);
 			if(hitInfo.collider != null && hitInfo.collider.gameObject != gameObject){
 				if(!hitted.Contains(hitInfo.collider.gameObject)){
 					hitted.Add(hitInfo.collider.gameObject);
@@ -69,12 +65,23 @@ public class ColdAction : PlayerActions {
 	override protected void DoAction() {
 		_startTime = Time.time;
 		_playerController.speedMultiplicator = .5f;
+		_isCharging = true;
+		Debug.Log("isChargin");
+	}
 
+	override protected void DoBehaviour(){
+		
+		if(_isCharging){
+			gabaritTransform.localScale = Vector3.one * Mathf.Clamp01((Time.time - _startTime)*chargeSpeed) * (_actionRange -3f);
+		}else{
+			gabaritTransform.localScale = Vector3.zero;
+		}
 	}
 
 	override protected void DoReleaseAction(){
 		_chargeAmount = Mathf.Clamp01((Time.time - _startTime)*chargeSpeed);
 		_playerController.speedMultiplicator = 1f;
+		_isCharging = false;
 		Fire();
 		GetComponent<Pusher>().Push(-transform.forward,.2f);
 
